894fc7295186734efd62978e37ced884b7f42a7b
Maxwell Foley <maxwellfoley@wustl.edu>
2019-03-12 22:51:24 -0500

    added additional error handling

--- app/containers/List/saga.js
+++ app/containers/List/saga.js
@@ -2,21 +2,17 @@ import { put, call, takeLatest } from 'redux-saga/effects';
 
 export function* databaseRead() {
   // read from the database here
-  const items = yield call(() =>
-    fetch('/api/list').then(response => response.json()),
-  );
-
-  // TODO: error handling
-  yield put({ type: 'SET_ITEMS', payload: items });
+  try {
+    const items = yield call(() =>
+      fetch('/api/list').then(response => response.json()),
+    );
+    yield put({ type: 'SET_ITEMS', payload: items });
+  } catch (e) {
+    // Error handling: if it can't connect, then it will display a single string alerting the user
+    yield put({ type: 'SET_ITEMS', payload: [{ item: 'An error occurred' }] });
+  }
 }
 
 export default function* watchDatabaseRead() {
   yield takeLatest('DATABASE_READ', databaseRead);
 }
-
-/*
-// single entry point to start all Sagas at once
-export default function* rootSaga() {
-  yield all([watchDatabaseRead(), watchDatabaseWrite()]);
-}
-*/
